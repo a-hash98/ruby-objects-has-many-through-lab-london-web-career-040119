451046d036bfa1e697df9389e9b1006677da98b1
Ay Has <ahashim983@yahoo.com>
2019-04-09 18:37:16 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,21 @@
+class Appointment
+  @@all = []
+
+  attr_accessor :date, :patient, :doctor
+
+  def self.all
+    @@all 
+  end
+
+  def initialize(date, patient, doctor)
+    @date = date 
+    @patient = patient 
+    @doctor = doctor 
+  end
+
+  
+    
+  
+
+  
+end
